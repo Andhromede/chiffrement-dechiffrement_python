2d210a893a9fed6c2cf3ce9cf2a66a23cc7ab25d
Andhromede <andhromede@hotmail.fr>
2023-06-05 20:06:46 +0200

    maj

--- chiffrement.py
+++ chiffrement.py
@@ -28,12 +28,9 @@ def dechiffrement(message, nbr):
 #=========================================================
 # DECHIFFREMENT
 # =========================================================
-
 messageAdechiffrer = "]\x8a\x89\x85\x8a\x90\x8d;\x88\x80\x8e;\x8f\x8dă\x8e;~\x83\x80\x8d\x8e;|\x8b\x8b\x8d\x80\x89|\x89\x8f\x8eI;aĄ\x87\x84~\x84\x8f|\x8f\x84\x8a\x89\x8e;û;\x91\x8a\x90\x8eG;\x91\x8a\x90\x8e;|\x91\x80\x95;\x8dĄ\x90\x8e\x8e\x84;û;~|\x8e\x8e\x80\x8d;~\x80;~\x8a\x7f\x80;<;e\x80;\x8e\x90\x84\x8e;\x81\x84\x80\x8d;\x7f\x80;\x91\x8a\x90\x8eI;h|\x84\x89\x8f\x80\x89|\x89\x8fG;\x91\x8a\x90\x8e;\x8b\x8a\x90\x91\x80\x95;~\x8a\x88\x88\x80\x89~\x80\x8d;\x87\x80;~\x8a\x7f\x80;\x7f\x80;q\x84\x82\x80\x89ă\x8d\x80"
 
 def calculFrequence(messageAdechiffrer):
-    # primaryKey = dechiffrement(messageAdechiffrer, 27)
-
     tab = {}
 
     for char in messageAdechiffrer:
@@ -41,21 +38,21 @@ def calculFrequence(messageAdechiffrer):
             tab[char] += 1
         else :
             tab[char] = 1
-       
 
+    # Permet de trouver le charactère le plus courant dans le dico
     lettreCourante = max(tab, key=tab.get)
-    print(lettreCourante)
-
+    # Convertit le caractère en ASCII
     asciiLettreCourante = ord(lettreCourante)
+    # Calcul l'écart (en ascii) entre le caractère courant et la barre espace
     ecart = - (ord(" ") - asciiLettreCourante)
-    print(ecart)
 
-    # return primaryKey
+    return dechiffrement(messageAdechiffrer, ecart)
 
 print(calculFrequence(messageAdechiffrer))
         
 
-
-
+#=========================================================
+# VIGENERE
+# =========================================================
 
 
