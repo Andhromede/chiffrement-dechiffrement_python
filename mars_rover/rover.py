c26c7018d97f26ea489b38880fabc1bf34cef045
Andhromede <andhromede@hotmail.fr>
2023-06-08 12:53:09 +0200

    exo mars rover terminé + ajout algo genetique

--- mars_rover/rover.py
+++ mars_rover/rover.py
@@ -1,9 +1,7 @@
 from math import *
 from itertools import permutations
 
-
-# formule: d = r2 (X2-X1)2 + (Y2-Y1)2 + (Z2-Z1)2
-
+# ============================================================
 dico = {
     0:[0, 0],
     1:[300.8, 202.54],
@@ -11,41 +9,33 @@ dico = {
     3:[65.9, 49.643],
 }
 
-# nb_possibilite = 2 * len(dico)
-# longueur = len(dico)
-# tmp= ""
-# liste = []
-
-
+# ============================================================
 # CALCUL TTES LES POSSIBILITES D'UNE CHAINE ET LES RENVOIS
-"""
-Renvoi un tableau contenant toutes les possibilités d'une chaine 
+# ============================================================
+def calcul_possibilites(dictionnaire, nbr):
+    """
+    Renvoi un tableau contenant toutes les possibilités d'une chaine 
    
-Args : 
+    Args : 
     dictionnaire : un tableau de chiffres ou de lettres
     nbr : le nombre de chiffres à afficher dans chacuns des résultats
 
-Ex :
+    Ex :
     tableau_initial = [1, 2, 3]
     resultat_fonction = [(1, 2, 3), (1, 3, 2), (2, 1, 3), (2, 3, 1), (3, 1, 2), (3, 2, 1)]
-"""
-def calcul_possibilites(dictionnaire, nbr):
+    """
+
     tableau_possibilite = []
     temp = permutations(dictionnaire, nbr)
     for i in list(temp):
-    	if (i[1] != 0):
+    	if (i[0] == 0):
             tableau_possibilite.append(i)
-            # print(tableau_possibilite)
     return tableau_possibilite
 
 
-# print(calcul_possibilites(dico, 4))
-
-
-
-
-
-# # CALCUL LES DISTANCES ENTRE DES POINTS
+# ============================================================
+# CALCUL LES DISTANCES ENTRE DES POINTS
+# ============================================================
 def calcul_distance(x1:float,y1:float,x2:float,y2:float) ->float:
     """Send back the distance between two points
 
@@ -58,57 +48,25 @@ def calcul_distance(x1:float,y1:float,x2:float,y2:float) ->float:
     Returns:
         float: Return the distance
     """
+
     distance = sqrt((x2-x1)**2 + (y2-y1)**2)
     return distance
 
 
+# ============================================================
+# CALCUL DES DISTANCES SELON TOUTES LES POSSIBILITES
+# ============================================================
+tab_possibilites = calcul_possibilites(dico, len(dico))
+result = 0
+tab_result={}
+
+for item in tab_possibilites :
+    for j in item:
+        result += calcul_distance(dico[item[j]][0], dico[item[j]][1], dico[item[j-1]][0], dico[item[j-1]][1])
+    tab_result[item] = result
+print(tab_result)
 
 
 
 
 
-
-
-
-
-
-# CALCUL LES DISTANCES ENTRE DES POINTS
-# def calcul_distance(xa, xb, ya, yb):
-#     DistanceAB=sqrt((xb-xa)**2+(yb-ya)**2)
-#     # print("la distance exacte entre A et B est de racine de",round(DistanceAB**2))
-#     # print("la distance approximative entre A et B est d'environ","%.2f" %(DistanceAB))
-#     return "%.2f" %(DistanceAB)
-
-
-
-last_number = 0
-tableau_distance = {}
-
-
-
-
-print(dico)
-
-for i in dico :
-    # print(i)
-    
-    for j in dico:
-        print(j)
-        last_number = dico[i]
-        distance = calcul_distance(dico[i][0], dico[i][0], last_number[0], last_number[1])
-        # distance = calcul_distance(dico[i][0], dico[i][0], last_number[0], last_number[1])
-
-        # if distance != 0:
-        tableau_distance[f"{i}/{i-1}"]= distance
-
-print(tableau_distance)
-
-
-
-
-
-# xa=float(300.8)
-# xb=float(202.54)
-# ya=float(5432.6)
-# yb=float(676.865)
-# print(calcul_distance(xa, xb, ya, yb))
